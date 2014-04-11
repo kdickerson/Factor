4153e5e7da15fe44b07f30bdd8820c3ae7f6aebe
Kyle Dickerson <kyle.dickerson@gmail.com>
2014-04-10 20:38:28 -0700

    Setup basic animation looper.

---
+++ js/setup.js
@@ -0,0 +1,34 @@
+// General setup and convenience functions
+
+var running = false;
+function run(tickFunction) {
+	var last = performance.now();
+	function step(timestamp) {
+		if (running) {
+			tickFunction(timestamp - last);
+			last = timestamp;
+			cancelID = requestAnimationFrame(step);
+		}
+	}
+	running = true;
+	cancelID = requestAnimationFrame(step); // start the first frame
+}
+
+function stop() {running = false;}
+
+
+
+
+function testRunner(pixelsPerSecond) {
+	var testDiv = document.getElementById('testDiv');
+	var w = testDiv.style.width = 0;
+	
+	run(function(delta) {
+		var widthDelta = (delta / 1000) * pixelsPerSecond;
+		w += widthDelta;
+		testDiv.style.width = w + 'px';
+		if (w > 500) {stop();}
+	});
+}
+testRunner(15);
+
